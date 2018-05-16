8dfb4044402d8da8071b3ec82f0dc84c2e4611b1
samdrudd <samdrudd@gmail.com>
2018-05-16 08:50:04 -0700

    Added satus to Job model, closes #1

--- app/models/JobModel.js
+++ app/models/JobModel.js
@@ -5,6 +5,10 @@ module.exports.jobSchema = mongoose.Schema({
 			type: Date,
 			required: true
 		},
+		status: {
+			type: String,
+			enum: ["Applied", "Interviewed", "Offer Received", "Rejected"]
+		},
 		title: {
 			type: String,
 			required: true
