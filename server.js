77839c150501b5749bd5287367048bfc5de5e42a
samdrudd <samdrudd@gmail.com>
2018-06-11 15:55:12 -0700

    fix origin

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port 			= process.env.PORT || 5000;
 
 
 app.use(cors({
-	origin : 'https://samdrudd.github.io/jobseeker/',
+	origin : 'https://samdrudd.github.io',
 	credentials : true
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
